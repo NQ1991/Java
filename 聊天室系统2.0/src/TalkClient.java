3091412f4965f5995a2b2b6505aa15dd26c77614
NQ1991 <nareoivy@gmail.com>
2014-10-25 20:49:56 +0800

    聊天室系统
    呵呵

---
+++ 聊天室系统2.0/src/TalkClient.java
@@ -0,0 +1,120 @@
+/*
+ * 多client界面
+ * Server不可界面
+ * client之间发消息
+ * 显示消息内容及日期
+ * 那强---2014-10-25
+ * 
+ * */
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class TalkClient extends Frame {
+	private static final long serialVersionUID = 5798410085795768914L;
+	public TextField tf = new TextField();
+	public TextArea ta = new TextArea();
+
+	DataOutputStream dos = null;
+	DataInputStream dis = null;
+
+	public Socket socket = null;
+
+	boolean connected = false;
+
+	Thread receiver = new Thread(new ReceiverThread());
+
+	public static void main(String[] args) {
+		new TalkClient().launchFrame();
+	}
+
+	public void launchFrame() {
+		this.setLocation(250, 100);
+		this.setSize(500, 600);
+		this.add(tf, BorderLayout.SOUTH);
+		this.add(ta, BorderLayout.NORTH);
+		this.pack();
+		this.addWindowListener(new WindowMonitor());
+		tf.addKeyListener(new KeyMonitor());
+		connect();
+		receiver.start();
+		this.setVisible(true);
+	}
+
+	public void connect() {
+		try {
+			socket = new Socket("127.0.0.1", 8888);
+			dos = new DataOutputStream(socket.getOutputStream());
+			dis = new DataInputStream(socket.getInputStream());
+			connected = true;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void disconnect() {
+		try {
+			dos.close();
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class WindowMonitor extends WindowAdapter {
+		@Override
+		public void windowClosing(WindowEvent e) {
+			disconnect();
+			System.exit(0);
+		}
+	}
+
+	private class KeyMonitor extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+			if (KeyEvent.VK_ENTER == keyCode) {
+				try {
+					dos.writeUTF(tf.getText().trim());
+					dos.flush();
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
+				}
+				tf.setText("");
+			}
+		}
+	}
+
+	private class ReceiverThread implements Runnable {
+
+		public void run() {
+			try {
+				while (connected) {
+					String str = dis.readUTF();
+					ta.setText(ta.getText() + str + '\n');
+				}
+			} catch (SocketException e) {
+				System.out.println("客户端断开连接!");
+				connected = false;
+			} catch (EOFException e) {
+				System.out.println("客户端断开连接!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ 聊天室系统2.0/src/TalkServer.java
@@ -0,0 +1,113 @@
+/*
+ * 多client界面
+ * Server不可界面
+ * client之间发消息
+ * 显示消息内容及日期
+ * 那强---2014-10-25
+ * 
+ * */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Vector;
+
+public class TalkServer {
+
+	Vector<Client> clients = new Vector<Client>();
+	
+	public static void main(String[] args) {
+		new TalkServer().start();
+	}
+
+	public void start() {
+		boolean listening = false;
+		ServerSocket ss = null;
+		int clientNumber=0;
+		try {
+			ss = new ServerSocket(8888);
+			listening = true;
+		} catch (BindException e) {
+			System.out.println("端口正在被使用，请关闭相应程序！");
+			System.exit(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			while (listening) {
+				Socket s = ss.accept();
+				Client c = new Client(s,clientNumber);
+				clients.add(c);
+				System.out.println("Client  "+clientNumber+" connected!");
+				new Thread(c).start();
+				clientNumber++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 每个请求过来启用一个线程来处理
+	 */
+	private class Client implements Runnable {
+		Socket socket = null;
+		int clientNumber=0;
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+		boolean connected = false;
+
+		Client(Socket s,int clientNumber) {
+			try {
+				this.socket = s;
+				this.clientNumber=clientNumber;
+				dis = new DataInputStream(s.getInputStream());
+				dos = new DataOutputStream(s.getOutputStream());
+				connected = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			while (connected) {//最大的问题是不同的线程中vector不同步，导致前面的client不能连接后面的client
+				try {
+					/*
+					Iterator<Client> it = clients.iterator();
+					String msg = dis.readUTF();
+					while (it.hasNext()) {
+						Client c = it.next();
+						c.send(msg,clientNumber);
+					}
+					*/
+					String msg = dis.readUTF();
+					for(int i=0;i<clients.size();i++){
+						clients.get(i).send(msg,clientNumber);
+					}
+
+				} catch (EOFException eof) {
+					connected = false;
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		private void send(String str,int clientNumber) throws IOException {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+			dos.writeUTF("Client"+clientNumber+"("+dateFormat.format(new Date()) +"):"+str);
+		}
+	}
+}
